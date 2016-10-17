8f92720ba09cdb9af0c8cc89094754186d7b6e73
JAvier <tirengarfio@gmail.com>
2016-10-17 11:11:34 +0200

    Added Tax rate to price field as a data attribute. Changed position fields

--- Form/ProductVersionType.php
+++ Form/ProductVersionType.php
@@ -19,12 +19,12 @@ class ProductVersionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('price_plus_taxes', TextType::class, array('label' => 'Precio (con I.V.A.)', 'mapped' => false, 'required' => false, 'attr' => array('class' => 'price_plus_taxes')))
-            ->add('price', null, array('label' => 'Precio (sin I.V.A.)', 'attr' => array('class' => 'price')))
-            ->add('color', null, array('label' => 'Color en filtros (dejar en blanco si no tiene un color especifico. Nombre de color en masculino. Atención acentos!!!!)', 'required' => false))
-            ->add('colorCode', null, array('label' => 'Color en web, por ejemplo escribe "negras" para "zapatillas negras"', 'required' => false))
+            ->add('colorCode', null, array('label' => 'Color en web (por ejemplo escribe "negras" para unas zapatillas negras)', 'required' => false))
             ->add('enabled', null, array('label' => 'Visible', 'required' => false))
             ->add('featured', null, array('label' => 'Destacado', 'required' => false))
+            ->add('price', null, array('label' => 'Precio (sin I.V.A.)', 'attr' => array('class' => 'price', 'data-tax-rate' => \Ziiweb\EcommerceBundle\Entity\TaxRates::VAT_RATE + 1)))
+            ->add('price_plus_taxes', TextType::class, array('label' => 'Precio (con I.V.A.)', 'mapped' => false, 'required' => false, 'attr' => array('class' => 'price_plus_taxes', 'data-tax-rate' => \Ziiweb\EcommerceBundle\Entity\TaxRates::VAT_RATE + 1)))
+            ->add('color', null, array('label' => 'Color en filtros de busqueda (poner "multicolor" si tienes varios colores. Nombre de color en masculino. Atención acentos!!!!)', 'required' => false))
             //->add('oldPrice')
             ->add('productVersionImages', CollectionType::class, array(
                 'entry_type' => ProductVersionImageType::class, 
