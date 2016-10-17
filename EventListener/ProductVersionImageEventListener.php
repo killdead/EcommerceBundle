dfcebfaf0c87fbac79e73379e024c88e591f80d2
JAvier <tirengarfio@gmail.com>
2016-10-17 12:20:02 +0200

    Check if a file has been uploaded before resize the image

--- EventListener/ProductVersionImageEventListener.php
+++ EventListener/ProductVersionImageEventListener.php
@@ -24,26 +24,28 @@ class ProductVersionImageEventListener
 
         if ($entity instanceof ProductVersionImage) {
 
-            $file = $entity->getFile();
+            if ($file = $entity->getFile() != null) {
+		$file = $entity->getFile();
 
-            $filename = md5(uniqid()) . '.' . $file->guessExtension(); 
+		$filename = md5(uniqid()) . '.' . $file->guessExtension(); 
 
-            $sizes = array('xs' => 65, 's' => 100, 'm' => 200, 'l' => 400, 'xl' => 600);
+		$sizes = array('xs' => 65, 's' => 100, 'm' => 200, 'l' => 400, 'xl' => 600);
 
-            foreach ($sizes as $key => $size) {
-                ${'imagick' . $key} = new \Imagick($file->getRealPath()); 
-		$newWidth = $size;
-                $newHeight = ${'imagick' . $key}->getImageHeight() * $newWidth / ${'imagick' . $key}->getImageWidth(); 
-		${'imagick' . $key}->resizeImage($newWidth, $newHeight, false, 1);  
-		//${'imagick' . $key}->setImageCompression(\Imagick::COMPRESSION_JPEG); 
-		//${'imagick' . $key}->setImageCompressionQuality(85);
-		${'imagick' . $key}->writeImage('uploads/' . $key . '/' . $filename);
+		foreach ($sizes as $key => $size) {
+		    ${'imagick' . $key} = new \Imagick($file->getRealPath()); 
+		    $newWidth = $size;
+		    $newHeight = ${'imagick' . $key}->getImageHeight() * $newWidth / ${'imagick' . $key}->getImageWidth(); 
+		    ${'imagick' . $key}->resizeImage($newWidth, $newHeight, false, 1);  
+		    //${'imagick' . $key}->setImageCompression(\Imagick::COMPRESSION_JPEG); 
+		    //${'imagick' . $key}->setImageCompressionQuality(85);
+		    ${'imagick' . $key}->writeImage('uploads/' . $key . '/' . $filename);
+		}
+		
+	     
+		$file->move('uploads', $filename);
+		
+		$entity->setFile($filename);
             }
-            
-         
-            $file->move('uploads', $filename);
-            
-            $entity->setFile($filename);
         }
     }
 }
