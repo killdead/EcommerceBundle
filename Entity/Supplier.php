53abb7ea55a34d2c0d92b318a2a57525fdd756c0
JAvier <tirengarfio@gmail.com>
2016-10-17 21:02:36 +0200

    Added supplier

---
+++ Entity/Supplier.php
@@ -0,0 +1,69 @@
+<?php
+ 
+namespace Ziiweb\EcommerceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Ziiweb\EcommerceBundle\Entity\Supplier
+ *
+ * @ORM\Table
+ * @ORM\Entity()
+ * @ORM\HasLifecycleCallbacks
+ */
+class Supplier
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="name")
+     */
+    protected $name;
+  
+    public function __toString() 
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Supplier
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
