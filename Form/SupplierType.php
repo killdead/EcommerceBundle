350abb536cac5db24fed1e8cc37d9b27a82b0662
JAvier <tirengarfio@gmail.com>
2016-10-17 23:06:13 +0200

    Added Submit field

--- Form/SupplierType.php
+++ Form/SupplierType.php
@@ -5,6 +5,7 @@ namespace Ziiweb\EcommerceBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SupplierType extends AbstractType
 {
